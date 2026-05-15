8938d20f55eca92cc02da9ccd29f55d2ac6a6766
Zaran <arcturus5271@gmail.com>
2026-05-15 16:13:57 +0700

    feat: Initialize self.walls, self.intersections, self.coins (set), and self.powerup_location

--- game_map.py
+++ game_map.py
@@ -2,9 +2,11 @@ import pygame
 from settings import *
 
 class Map:
-     def __init__(self, map_walls=None, map_intersections=None):
-        # TODO 6: Initialize self.walls, self.intersections, self.coins (set), and self.powerup_location
-        pass
+     def __init__(self, map_walls = [[0 for i in range(GRID_WIDTH)] for j in range(GRID_HEIGHT)], map_intersections = [[0 for i in range(GRID_WIDTH)] for j in range(GRID_HEIGHT)]):
+        self.walls = map_walls
+        self.intersections = map_intersections
+        self.coins = set()
+        self.powerup_location = None
 
      # TODO 7: Implement add_coin, remove_coin, set_powerup, and remove_powerup logic
      def is_wall(self, cell_x, cell_y):
